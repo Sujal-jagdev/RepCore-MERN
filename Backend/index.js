27f5002e6b8cad0572c145d8987511d05450f381
Sujal Jagdev <rg2844537@gmail.com>
2025-09-15 16:53:13 +0530

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const isLoggedIn = require("./Middlewares/isLoggedin")
 
 app.use(express.json())
 app.use(cors({ 
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL, 
     credentials: true 
 }));
 app.use(cookieParser())
